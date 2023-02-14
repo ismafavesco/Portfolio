77fd02b9770918932d40843b4aefcb28d8d6a470
ismael favela <77935416+ismafavesco@users.noreply.github.com>
2023-02-14 14:13:55 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: 'class',
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      fontFamily: {
+        'myfont': ['Josefin Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
