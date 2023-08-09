3441d8367121b3455b239134e8eeda46e7b6bc87
ismael favela <77935416+ismafavesco@users.noreply.github.com>
2023-08-09 16:44:17 -0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <div className="bg-sky-100 dark:bg-slate-900 dark:text-white p-4 font-myfont">
       <p className="text-base text-center">
-        Copyright &copy; {copyrightDate}  | All Rights Reserved
+        Copyright &copy; {copyrightDate}  | Ismael All Rights Reserved
       </p>
     </div>
   );
