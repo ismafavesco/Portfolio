359c2b0f522d9a1f33ae9f0c0bc80d77d8b818c3
ismael favela <77935416+ismafavesco@users.noreply.github.com>
2023-10-25 17:28:30 -0700

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -45,27 +45,27 @@ const Work = () => {
             </div>
           </div>
           {/* Works Container Ends */}
+
           {/* Project 2 Starts */}
           <div
             style={{
               backgroundImage:
-              'url("https://i.postimg.cc/Gtxjtkyd/favesocial.png',
+              'url("https://i.postimg.cc/FH0C39W2/gymdex.png',
               backgroundRepeat: "no-repeat",
-              backgroundPosition: "center ",
-              backgroundSize: "cover ",
-
+              backgroundPosition: "center",
+              backgroundSize: "cover",
             }}
             className="w-full md:h-60 h-48 group rounded-md shadow-lg overflow-hidden shadow-[#040c16]"
           >
             <div className="w-full h-full bg-sky-700 opacity-0 hover:opacity-100 duration-300">
               <div className="relative p-2 md:p-8 h-full w-full text-2xl text-white font-bold">
-                <p>Favesocial</p>
+                <p>Gymdex</p>
                 <p className="text-xl font-normal text-gray-200 mt-2">
-               Basic semi-responsive social media website made with HTML, CSS and JavaScript (2020)
+                Gymdex is a user-friendly platform designed to provide Gym enthusiasts access to thousands of exercises on demand. (2023)
                 </p>
                 <div className="absolute right-2 bottom-2">
                   <a
-                    href="https://ismafavesco.github.io/Favesocial/"
+                    href="https://gymdex.vercel.app/"
                     className="cursor-pointer block"
                     target={"_blank"}
                   >
@@ -76,7 +76,7 @@ const Work = () => {
             </div>
           </div>
           {/* Works Container Ends */}
-          {/* Project 3 Starts */}
+ {/* Project 3 Starts */}
           <div
             style={{
               backgroundImage:
@@ -107,7 +107,42 @@ const Work = () => {
             </div>
           </div>
           {/* Works Container Ends */}
-          {/* Project 4 starts */}
+         
+        {/* Project 4 Starts */}
+          <div
+            style={{
+              backgroundImage:
+              'url("https://i.postimg.cc/Gtxjtkyd/favesocial.png',
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center ",
+              backgroundSize: "cover ",
+
+            }}
+            className="w-full md:h-60 h-48 group rounded-md shadow-lg overflow-hidden shadow-[#040c16]"
+          >
+            <div className="w-full h-full bg-sky-700 opacity-0 hover:opacity-100 duration-300">
+              <div className="relative p-2 md:p-8 h-full w-full text-2xl text-white font-bold">
+                <p>Favesocial</p>
+                <p className="text-xl font-normal text-gray-200 mt-2">
+               Basic semi-responsive social media website made with HTML, CSS and JavaScript (2020)
+                </p>
+                <div className="absolute right-2 bottom-2">
+                  <a
+                    href="https://ismafavesco.github.io/Favesocial/"
+                    className="cursor-pointer block"
+                    target={"_blank"}
+                  >
+                    <FaDownload className="cursor-pointer text-5xl rounded-full p-1 hover:bg-transparent hover:text-white border-4 bg-white border-white text-sky-700 duration-300" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Works Container Ends */}
+
+
+          
+          {/* Project 5 starts */}
           <div
             style={{
               backgroundImage:
@@ -136,7 +171,7 @@ const Work = () => {
               </div>
             </div>
           </div>
-          {/* Project 5 starts */}
+          {/* Project 6 starts */}
           <div
             style={{
               backgroundImage:
